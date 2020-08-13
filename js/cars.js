8adbc9ba39766d3d22fc837c4daee48d177c4558
Chris Thompson <chris.thompson447@gmail.com>
2020-08-13 18:05:58 +0100

    updated cars data and logic for rendering

--- js/cars.js
+++ js/cars.js
@@ -1,16 +1,19 @@
+//declaring cars variable to assign json object to
 var cars;
 
+//Using jquery to parse local json file into a javascript object.
 $.getJSON("json/cars.json", function (data) {
     cars = data;
 });
 
+//for loop, this will render each json object as it's own car.
 function renderCars() {
     for(var i=0; i < cars.cars.length; i++) {
-       console.log(cars.cars[i]);
         renderCards(cars.cars[i]);
    }
 }
 
+// attach our cards to html parent element with cardAnchor id.
 function renderCards(car) {
     const cardAnchor = document.getElementById('cardAnchor');
 
@@ -18,16 +21,25 @@ function renderCards(car) {
 
 }
 
+//This method builds up a card object using the materialize classes.
 function renderSingleCard(car) {
     const card = document.createElement('div');
-    const cardContainer = document.createElement('div');
-    const cardImage = document.createElement('img');
-    const carName = document.createElement('span');
-    const cardContent = document.createElement('div');
 
     card.className = "card";
 
-    cardContainer.className = "card-image";
+    card.appendChild(renderCardImageAndTitle(car));
+    card.appendChild(renderCardContent(car));
+    card.appendChild(renderCardAction());
+
+    return card;
+}
+
+function renderCardImageAndTitle(car) {
+    const cardImageContainer = document.createElement('div');
+    const cardImage = document.createElement('img');
+    const carName = document.createElement('span');
+
+    cardImageContainer.className = "card-image";
 
     cardImage.src =car.images[0];
     cardImage.height ="600";
@@ -36,14 +48,53 @@ function renderSingleCard(car) {
     carName.className = "card-title";
     carName.innerHTML = car.make + " " + car.model;
 
-    cardContent.className = "card-content";
+    cardImageContainer.appendChild(cardImage);
+    cardImageContainer.appendChild(carName);
 
-    cardContainer.appendChild(cardImage);
-    cardContainer.appendChild(carName);
-    cardContainer.appendChild(cardContent);
+    return cardImageContainer;
+}
 
-    card.appendChild(cardContainer);
+function renderCardContent(car) {
+    const cardContentContainer = document.createElement('div');
+    const cardParagraph = document.createElement('p');
 
+    cardContentContainer.className = "card-content";
 
-    return card;
+    cardParagraph.innerHTML=car.description;
+    cardParagraph.innerHTML+='<br/> Fuel type:';
+
+    //Conditionally decide which icon to display if car is petrol/diesel or electric
+    if(car.fuelType != "electric"){
+        cardParagraph.innerHTML+=' ' + car.fuelType;
+        cardParagraph.innerHTML+='<i class="material-icons">local_gas_station</i>'
+    } else{
+        cardParagraph.innerHTML+=car.fuelType;
+        cardParagraph.innerHTML+='<i class="material-icons">battery_charging_full</i>'
+    }
+
+    cardParagraph.innerHTML+=('<br/> Daily Rate: &pound' + car.dailyRate);
+
+    cardContentContainer.appendChild(cardParagraph);
+
+    return cardContentContainer
+}
+
+function renderCardAction() {
+    const cardAction = document.createElement('div');
+    const cardActionButton = document.createElement('button');
+
+    cardAction.className="card-action right-align";
+    cardActionButton.type="button";
+    cardActionButton.id="carHireSubmitButton";
+    cardActionButton.className="btn waves-effect waves-light";
+    cardActionButton.onclick=submitCarChoice;
+    cardActionButton.innerHTML="Hire this car";
+    cardActionButton.innerHTML+="<i class=\"material-icons right\">send</i>";
+    cardAction.appendChild(cardActionButton);
+
+    return cardAction;
+}
+
+function submitCarChoice() {
+    alert('car submitted');
 }
