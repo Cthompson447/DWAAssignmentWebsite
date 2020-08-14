945e4ae2a3ce2b6412aa469bb419bbfdaaec151c
Chris Thompson <chris.thompson447@gmail.com>
2020-08-14 06:14:01 +0100

    Added template pages for confirm and success hire. Also updated cars logic to render based off of age parameter and formatting. Responsiveness added to car selection page.

--- js/cars.js
+++ js/cars.js
@@ -1,5 +1,6 @@
 //declaring cars variable to assign json object to
 var cars;
+const isOver25 = localStorage.getItem('isOver25');
 
 //Using jquery to parse local json file into a javascript object.
 $.getJSON("json/cars.json", function (data) {
@@ -8,16 +9,22 @@ $.getJSON("json/cars.json", function (data) {
 
 //for loop, this will render each json object as it's own car.
 function renderCars() {
-    for(var i=0; i < cars.cars.length; i++) {
+    for (var i = 0; i < cars.cars.length; i++) {
         renderCards(cars.cars[i]);
-   }
+    }
 }
 
 // attach our cards to html parent element with cardAnchor id.
 function renderCards(car) {
     const cardAnchor = document.getElementById('cardAnchor');
 
-    cardAnchor.append(renderSingleCard(car));
+    /*If user under 25, render cars available for under 25 hire. If user is over 25, render cars that are unavailable
+    for those under 25.*/
+    if(car.isAvailableForUnder25Hire){
+        cardAnchor.append(renderSingleCard(car));
+    } else if (!car.isAvailableForUnder25Hire && isOver25 == "true"){
+        cardAnchor.append(renderSingleCard(car));
+    }
 
 }
 
@@ -29,11 +36,13 @@ function renderSingleCard(car) {
 
     card.appendChild(renderCardImageAndTitle(car));
     card.appendChild(renderCardContent(car));
-    card.appendChild(renderCardAction());
+    card.appendChild(renderCardAction(car));
 
     return card;
 }
 
+/*Does what it says on the tin, this method creates the image section of the card, populates image with first car
+ image and adds title through use of a basic string builder. All data is populated from the json.*/
 function renderCardImageAndTitle(car) {
     const cardImageContainer = document.createElement('div');
     const cardImage = document.createElement('img');
@@ -41,9 +50,13 @@ function renderCardImageAndTitle(car) {
 
     cardImageContainer.className = "card-image";
 
-    cardImage.src =car.images[0];
-    cardImage.height ="600";
-    cardImage.width ="400";
+    cardImage.src = car.images[0];
+    cardImage.height = "450";
+    cardImage.width = "400";
+
+    /*During development of this site, I have run lighthouse audits often to highlight shortcomings in my development
+    running lighthouse gave poor accessibility for this page so I've made this string builder to alt tag each image*/
+    cardImage.alt = "Image of a " + car.make + " " + car.model;
 
     carName.className = "card-title";
     carName.innerHTML = car.make + " " + car.model;
@@ -54,47 +67,54 @@ function renderCardImageAndTitle(car) {
     return cardImageContainer;
 }
 
+/*This method creates the content section of the card, same as above, this method manipulates the dom structure to add
+a div and paragraph element and populate these from the json. This method also handles conditional display of icons
+ depending on fuel type of car.*/
 function renderCardContent(car) {
     const cardContentContainer = document.createElement('div');
     const cardParagraph = document.createElement('p');
 
     cardContentContainer.className = "card-content";
 
-    cardParagraph.innerHTML=car.description;
-    cardParagraph.innerHTML+='<br/> Fuel type:';
+    cardParagraph.innerHTML = car.description;
+    cardParagraph.innerHTML += '<br/> Fuel type:';
 
     //Conditionally decide which icon to display if car is petrol/diesel or electric
-    if(car.fuelType != "electric"){
-        cardParagraph.innerHTML+=' ' + car.fuelType;
-        cardParagraph.innerHTML+='<i class="material-icons">local_gas_station</i>'
-    } else{
-        cardParagraph.innerHTML+=car.fuelType;
-        cardParagraph.innerHTML+='<i class="material-icons">battery_charging_full</i>'
+    if (car.fuelType != "Electric") {
+        cardParagraph.innerHTML += ' ' + car.fuelType;
+        cardParagraph.innerHTML += '<i class="material-icons">local_gas_station</i>'
+    } else {
+        cardParagraph.innerHTML += car.fuelType;
+        cardParagraph.innerHTML += '<i class="material-icons">battery_charging_full</i>'
     }
 
-    cardParagraph.innerHTML+=('<br/> Daily Rate: &pound' + car.dailyRate);
+    cardParagraph.innerHTML += ('<br/> Daily Rate: &pound' + car.dailyRate);
 
     cardContentContainer.appendChild(cardParagraph);
 
     return cardContentContainer
 }
 
-function renderCardAction() {
+/*This method draws the stub at the bottom of each card with the button. All this method does is create the
+stub, add the button with submitCarChoice as button action, and add the submit icon to the button.*/
+function renderCardAction(car) {
     const cardAction = document.createElement('div');
     const cardActionButton = document.createElement('button');
 
-    cardAction.className="card-action right-align";
-    cardActionButton.type="button";
-    cardActionButton.id="carHireSubmitButton";
-    cardActionButton.className="btn waves-effect waves-light";
-    cardActionButton.onclick=submitCarChoice;
-    cardActionButton.innerHTML="Hire this car";
-    cardActionButton.innerHTML+="<i class=\"material-icons right\">send</i>";
+    cardAction.className = "card-action right-align";
+    cardActionButton.type = "button";
+    cardActionButton.className = "btn waves-effect waves-light car-hire-submit-button";
+    cardActionButton.setAttribute('onclick', 'submitCarChoice(' + car.id + ')');
+    cardActionButton.innerHTML = "Hire this car";
+    cardActionButton.innerHTML += "<i class=\"material-icons right\">send</i>";
     cardAction.appendChild(cardActionButton);
 
     return cardAction;
 }
 
-function submitCarChoice() {
-    alert('car submitted');
+/*Next page is an order page, to save parsing json data, we can simply just save the car we want for our transaction
+in local storage and use that object for calculations/fields there*/
+function submitCarChoice(carID) {
+    localStorage.setItem('selectedCar', JSON.stringify(cars.cars[carID - 1]));
+    window.location.href = ("confirmCarHire.html");
 }
